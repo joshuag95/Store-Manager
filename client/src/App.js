556dc1e8a777e82fc5dcec23bb06df12c50a1182
Joshua Greer <jtgreer95@gmail.com>
2022-12-13 20:44:53 -0600

    App Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,47 @@
 
 import './App.css';
+import {sseState, useEffect} from 'react'
+import {Switch, Route} from 'react-router-dom'
+
 
 function App() {
+
+  const [storeData , setStoreData] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/stores')
+    .then(resp => resp.json())
+    .then(data => setStoreData(data))
+  },[])
+
+  const createNewStore = (obj) => {
+    setStoreData( storeData => [...storeData, obj] )
+  }
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        Learn React
-       
-      </header>
+      
+      <NavBar />
+
+      <Switch>
+
+        <Route exact path="/" >
+          <Home />
+        </Route>
+
+        <Route exact path="/stores" >
+          <StoresPage storeData={storeData} createNewStore={createNewStore} />
+        </Route>
+
+        <Route exact path="/stores/:id">
+          <StoreCard storeData={storeData} />
+        </Route>
+
+      </Switch>
+    
     </div>
   );
 }
+
 
 export default App;
